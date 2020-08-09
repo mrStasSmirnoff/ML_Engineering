1dd1cf8c18b5f95bde6ae3257a4d9d06be4a54bb
ssmirnov <stanislav.smirnov@cleverbridge.com>
2020-08-09 18:25:30 +0200

    wrapping in docker file and creating Flask app

---
+++ REST_API_home_automation/device_registry/__init__.py
@@ -0,0 +1,18 @@
+import markdown
+import os
+from flask import Flask
+
+# Create an instance fo Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """Present some documentation"""
+
+    with open(os.path.dirname(app.root_path) + '/README.md', 'r') as markdown_file:
+
+        #Read the content of the file
+        content = markdown_file.read()
+
+        #Convert to HTML
+        return markdown.markdown(content)
