2a20c01291c200cd35713187dce0cf1f9272e5d0
Stanislav Smirnov <mr.stassmirnoff@hotmail.com>
2020-08-12 00:44:08 +0200

    the same

--- REST_API_home_automation/device_registry/__init__.py
+++ REST_API_home_automation/device_registry/__init__.py
@@ -1,40 +1,42 @@
 import markdown
 import os
-from flask import Flask, g 
 import shelve
+from flask import Flask, g
+from flask_restful import Resource, Api
 
 # Create an instance fo Flask
 app = Flask(__name__)
 
+# Create the API
+api = Api(app)
 
 def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = shelve.open("devices.db")
-    return g.sqlite_db
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = shelve.open("devices.db")
+    return db
 
 @app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route("/")
 def index():
     """Present some documentation"""
 
+    # Open the README file
     with open(os.path.dirname(app.root_path) + '/README.md', 'r') as markdown_file:
 
-        #Read the content of the file
+        # Read the content of the file
         content = markdown_file.read()
 
-        #Convert to HTML
+        # Convert to HTML
         return markdown.markdown(content)
 
-class DeviceList():
-    
+
+class DeviceList(Resource):
     def get(self):
         shelf = get_db()
         keys = list(shelf.keys())
@@ -43,5 +45,8 @@ class DeviceList():
 
         for key in keys:
             devices.append(shelf[key])
-        
-        return {'message': 'Success', 'data': devices}
+
+        return {'message': 'Success', 'data': devices}, 200
+
+
+api.add_resource(DeviceList, '/devices')
