e5ff1f02b0c8f287b63a1a8acc88000cb7b770da
Stanislav Smirnov <mr.stassmirnoff@hotmail.com>
2020-08-16 15:59:51 +0200

    adding post method and adding device class

--- REST_API_home_automation/device_registry/__init__.py
+++ REST_API_home_automation/device_registry/__init__.py
@@ -1,10 +1,12 @@
 import markdown
 import os
 import shelve
-from flask import Flask, g
-from flask_restful import Resource, Api
 
-# Create an instance fo Flask
+# Import the framework
+from flask import Flask, g
+from flask_restful import Resource, Api, reqparse
+
+# Create an instance of Flask
 app = Flask(__name__)
 
 # Create the API
@@ -48,5 +50,43 @@ class DeviceList(Resource):
 
         return {'message': 'Success', 'data': devices}, 200
 
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('identifier', required=True)
+        parser.add_argument('name', required=True)
+        parser.add_argument('device_type', required=True)
+        parser.add_argument('controller_gateway', required=True)
+
+        # Parse the arguments into an object
+        args = parser.parse_args()
+
+        shelf = get_db()
+        shelf[args['identifier']] = args
+
+        return {'message': 'Device registered', 'data': args}, 201
+
+
+class Device(Resource):
+    def get(self, identifier):
+        shelf = get_db()
+
+        # If the key does not exist in the data store, return a 404 error.
+        if not (identifier in shelf):
+            return {'message': 'Device not found', 'data': {}}, 404
+
+        return {'message': 'Device found', 'data': shelf[identifier]}, 200
+
+    def delete(self, identifier):
+        shelf = get_db()
+
+        # If the key does not exist in the data store, return a 404 error.
+        if not (identifier in shelf):
+            return {'message': 'Device not found', 'data': {}}, 404
+
+        del shelf[identifier]
+        return '', 204
+
 
 api.add_resource(DeviceList, '/devices')
+api.add_resource(Device, '/device/<string:identifier>')
