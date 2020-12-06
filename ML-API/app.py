4b50e0d1e03f3729fb4e9ab3299919f4274411e3
Stanislav Smirnov <mr.stassmirnoff@hotmail.com>
2020-12-06 18:21:05 +0100

    tested app locally

--- ML-API/app.py
+++ ML-API/app.py
@@ -6,12 +6,18 @@ from helper_functions import preprocess
 
 app = Flask(__name__)
 
-
+# this is a simple test to check whether app is working. It should return a simple string.
 @app.route("/", methods=["GET"])
 def hello():
     return jsonify("hello from ML API")
 
-
+# if someone sends a get request to this URL of our flask application along with raw data in the form of JSON, 
+# we will preprocess the data the same way we did for creating the model, get predictions and send back the prediction results.
+# request.get_json() will basically give us the JSON data that was sent with the get request. We convert that data into a dataframe, 
+# use our helper function preprocess() to preprocess the dataframe, use the model_name and column names from the config file to basically 
+# load the model with pickle and make predictions on the sliced dataframe. After making the predictions, we will create a response dictionary 
+# that contains predictions and prediction label metadata and finally convert that to JSON using jsonify and return the JSON back. 
+# Remember that 200 is sent as it was a success.
 @app.route("/predictions", methods=["GET"])
 def predictions():
     data = request.get_json()
