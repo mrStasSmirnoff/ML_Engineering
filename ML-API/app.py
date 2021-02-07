746962aad3b76115864544d0e35bcfa4f9dbe4cb
mrStasSmirnoff <mr.StasSmirnoff@hotmail.com>
2021-02-07 21:43:06 +0100

    Some updates on code, env etc

--- ML-API/app.py
+++ ML-API/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request, make_response
 import pandas as pd
+import numpy as np
+import sklearn
 import pickle
+import json
 from config import *
 from helper_functions import preprocess
 
